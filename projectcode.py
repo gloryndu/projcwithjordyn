4e4cfdf4991f40e370b71644bc5b85a52b891280
gloryndu <endu@terpmail.umd.edu>
2024-04-29 20:10:49 -0700

    testing

--- projectcode.py
+++ projectcode.py
@@ -1,1 +1,14 @@
-y = 1
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.processing import image
+import tensorflow  as tf 
+import matplotlib.pyplot as plt 
+import cv2 
+import os
+import numpy as np 
+
+img = image.load_img("")
+plt.imshow(img)
+cv2.imread("").shape
+
+train = ImageDataGenerator(rescale= 1/255)
+vaildation = ImageDataGenerator(rescale= 1/255)
