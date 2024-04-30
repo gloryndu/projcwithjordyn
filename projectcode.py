e41c50f740e062834871e2391fa8636883c910e2
gloryndu <endu@terpmail.umd.edu>
2024-04-29 20:35:41 -0700

    adding

--- projectcode.py
+++ projectcode.py
@@ -21,3 +21,19 @@ vaildation_dataset = train.flow_from_directory("",
                                           target_size=(200,200),
                                           batch_size = 3,
                                           class_mode="binary")
+
+model = tf.keras.models.Sequential([tf.keras.layers.Conv2D(16,(3,3), activation = 'relu',input_shape=(200,200,3)),
+                                    tf.keras.layers.MaxPool2D(2,2),
+                                    #
+                                    tf.keras.layers.Conv2D(32(3,3), activation = 'relu'),
+                                    tf.keras.layers.MaxPool2D(2,2)
+                                    #
+                                    tf.keras.layers.Conv2D(64(3,3), activation = 'relu'),
+                                    tf.keras.layers.MaxPool2D(2,2)
+                                    ##
+                                    tf.keras.layers.Flatten(),
+                                    ##
+                                    tf.keras.layers.Dense(512,activation= 'relu')
+                                    ##
+                                    tf.keras.layers.Dense(1,activation='sigmoid')
+                                    ])
