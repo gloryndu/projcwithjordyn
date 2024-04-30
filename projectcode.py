215d95935be2703919982824e4e2b1cc59fbc748
gloryndu <endu@terpmail.umd.edu>
2024-04-29 20:37:28 -0700

    https://github.com/gloryndu/projcwithjordyn.git

--- projectcode.py
+++ projectcode.py
@@ -26,14 +26,14 @@ model = tf.keras.models.Sequential([tf.keras.layers.Conv2D(16,(3,3), activation 
                                     tf.keras.layers.MaxPool2D(2,2),
                                     #
                                     tf.keras.layers.Conv2D(32(3,3), activation = 'relu'),
-                                    tf.keras.layers.MaxPool2D(2,2)
+                                    tf.keras.layers.MaxPool2D(2,2),
                                     #
                                     tf.keras.layers.Conv2D(64(3,3), activation = 'relu'),
-                                    tf.keras.layers.MaxPool2D(2,2)
+                                    tf.keras.layers.MaxPool2D(2,2),
                                     ##
                                     tf.keras.layers.Flatten(),
                                     ##
-                                    tf.keras.layers.Dense(512,activation= 'relu')
+                                    tf.keras.layers.Dense(512,activation= 'relu'),
                                     ##
                                     tf.keras.layers.Dense(1,activation='sigmoid')
                                     ])
